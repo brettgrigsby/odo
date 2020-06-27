3e8e596fea91480b8aff0e279bf57383320eca49
brettgrigsby <gbrett@gmail.com>
2020-06-27 13:46:06 -0500

    optional arg

--- components/SceneFromImages.tsx
+++ components/SceneFromImages.tsx
@@ -2,11 +2,11 @@ import { docFromImages } from '../utils/sceneBuilder'
 
 type IframePageProps = {
     imageFilenames: string[],
-    path: string
+    path?: string
 }
 
 const IframePage = (props: IframePageProps) => {
-    const src = docFromImages(props.imageFilenames, props.path)
+    const src = docFromImages(props.imageFilenames, props.path || '')
     return (
         <iframe style={{height: '100vh', width: '100vw' }} srcDoc={src} frameBorder='0' />
     )
