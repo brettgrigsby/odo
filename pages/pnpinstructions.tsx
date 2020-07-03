c1b7375147d8f5799d1f8c32588c239b57ade444
brettgrigsby <gbrett@gmail.com>
2020-07-02 21:08:37 -0500

    start of pnp page

---
+++ pages/pnpinstructions.tsx
@@ -0,0 +1,12 @@
+const PnpinstructionsPage = () => {
+    return (
+        <div>
+            <p>
+                Here's a <a href="/odo-deck.zip">zip file</a> with all of the
+                card images you'll need to print your own ODO deck.
+            </p>
+        </div>
+    )
+}
+
+export default PnpinstructionsPage
